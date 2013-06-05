ff0a70a5476f73bd0423708716e1f83b117d6243
ArpanBom <arpanbom@gmail.com>
2013-06-05 23:35:53 +0545

    Create jquery_form.js

---
+++ jquery_form.js
@@ -0,0 +1,44 @@
+
+//
+//  Prepared by David Cochran
+//
+//	Free for your use -- No warranties, no guarantees!
+//
+
+$(document).ready(function(){
+
+	// Validate
+	// http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	// http://docs.jquery.com/Plugins/Validation/
+	// http://docs.jquery.com/Plugins/Validation/validate#toptions
+
+		$('#purchaseForm').validate({
+	    rules: {
+	      name: {
+	        minlength: 2,
+	        required: true
+	      },
+	      email: {
+	        required: true,
+	        email: true
+	      },
+	      shippingaddress: {
+	      	minlength: 2,
+	        required: true
+	      },
+	      message: {
+	        minlength: 2,
+	        required: true
+	      }
+	    },
+			highlight: function(element) {
+				$(element).closest('.control-group').removeClass('success').addClass('error');
+			},
+			success: function(element) {
+				element
+				//.text('OK!').addClass('valid')
+				.closest('.control-group').removeClass('error').addClass('success');
+			}
+	  });
+
+}); // end document.ready
